6a12eb8bd3fa0e32fa08a3d7d7a827fc7a5620f6
Leon Kos <leon.kos@lecad.fs.uni-lj.si>
2019-03-18 00:15:16 +0100

    Build is OK even if there is no change in HTML. Rare config case

--- .circleci/run-build-locally.sh
+++ .circleci/run-build-locally.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
+set -ex
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
     --form config=@config.yml \
     --form notify=false \
-        https://circleci.com/api/v1.1/project/github/WPCD-Workflows/documentation/tree/masetr
+        https://circleci.com/api/v1.1/project/github/WPCD-Workflows/documentation/tree/master
 #        https://circleci.com/api/v1.1/project/<source, eg. github>/<user name>/<project name>/tree/<branch name>
 #
 #    --form revision=<commit hash>\
